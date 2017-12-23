ce70753e666c8d16c2a8fc024019b316cc066f93
Govindarajan Vishnuchithan <govindarajan.vishnuchithan@altisource.com>
2017-12-23 15:09:18 +0530

    Changes done

---
+++ batch_pull.sh
@@ -0,0 +1,5 @@
+cd shell && git pull origin master
+cd ../chef && git pull origin master
+cd ../ruby && git pull origin master
+cd ../python && git pull origin master
+cd ../shared && git pull origin master
---
+++ batch_push.sh
@@ -0,0 +1,5 @@
+cd shell && git push origin master
+cd ../chef && git push origin master
+cd ../ruby && git push origin master
+cd ../python && git push origin master
+cd ../shared && git push origin master
